0aa084af5cb0aff14dc77ec1f3bdc03de1875f1a
AimerSaigo <76782842+AimerSaigo@users.noreply.github.com>
2023-01-07 22:54:23 +0800

    Update shops.vue

--- src/views/business/shops.vue
+++ src/views/business/shops.vue
@@ -1,34 +1,52 @@
 <template>
-
+  <div class="business-shops">
+    <el-card v-for="shop in shops" :key="shop" @click="handleDetailClick(shop.id)" >
+      {{ shop.name }}
+      {{ shop.location }}
+    </el-card>
+  </div>
 </template>
 
 <script>
 import businessApi from '@/api/business'
+import axios from 'axios'
 
 export default {
   name: 'Shops',
   data() {
     return {
-      businessId: '',
+      businessId: this.$store.getters.businessId,
       shopId: '',
       name: '',
       location: '',
-      shops: []
+      shops: [],
+      dialogFormVisible: false,
+      form: {
+        name: '',
+        location: ''
+      },
+      formLabelWidth: '120px'
     }
   },
   created() {
   },
   methods: {
     getallShops() {
-      businessApi.getShops(this.$store.state.businessId).then(res => {
+      axios.post('/business/getShop', this.businessId).then(res => {
         this.shops = res.data
       })
     },
-    handleDetailClick(id) {
-      this.$router.push('/shops/${id}')
+    handleDetailClick(shopId) {
+      this.$store.commit('user/SET_SHOPID', shopId)
+      this.$router.push('/business/shopDetails')
     },
     handleCreateClick() {
-      businessApi.addShop(this.businessId, this.location, this.name)
+      axios.post('/shop/addShop', {
+        businessId: this.businessId,
+        location: this.location,
+        name: this,name
+      })
+      this.dialogFormVisible = false
       this.getallShops()
     }
   }
