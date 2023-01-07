3b8f6e79ac46e32aee5d4819e37525be8f9bb81f
AimerSaigo <76782842+AimerSaigo@users.noreply.github.com>
2023-01-07 22:55:34 +0800

    Update shopDetails.vue

--- src/views/customer/shopDetails.vue
+++ src/views/customer/shopDetails.vue
@@ -1,9 +1,19 @@
 <template>
-
+  <div class="customer-shopDetails">
+    <el-card v-for="good in goods" :key="good">
+      {{ good.name }}
+      {{ good.price }}
+      {{ good.status}}
+      <el-button type="primary" @click="handlePick(good)">
+        +1
+      </el-button>
+    </el-card>
+  </div>
 </template>
 
 <script>
 import customerApi from '@/api/customer'
+import axios from 'axios'
 
 export default {
   name: 'ShopDetails',
@@ -13,7 +23,7 @@ export default {
       name: '',
       price: '',
       status: '',
-      shopId: '',
+      shopId: this.$store.getters.shopId,
       goods: [],
       quality: '',
       customerId: '',
@@ -24,13 +34,26 @@ export default {
     this.getallGoods()
   },
   methods: {
+    // getallGoods() {
+    //   customerApi.getGoods(this.$store.state.shopId).then(res => {
+    //     this.goods = res.data
+    //   })
+    // },
     getallGoods() {
-      customerApi.getGoods(this.shopId).then(res => {
-        this.info = res.data
+      console.log(Number(this.shopId))
+      axios.post('/shop/getGoods', Number(this.shopId)).then(res => {
+        this.goods = res.data
+        console.log(this.goods)
+        console.log(res.data)
       })
     },
-    pickGoods() {
-      customerApi.placeOrder(this.goods[this.goodsId])
+    handlePick(good) {
+      axios.put('/order/placeOrder',{
+        customer.id: this.$store.getters.customerId,
+        goods.id: good.id,
+        quality: 1,
+        shop.id: this.$store.getters.shopId
+      })
     }
   }
 }
